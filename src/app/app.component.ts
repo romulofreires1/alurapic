e9f91a45e3e728b20b78b09fa40ac4df506427cd
Romulo <romulofreires1@gmail.com>
2019-04-15 12:47:43 -0300

    adding bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Lion_%28Panthera_leo%29_%2830941994012%29.jpg/1920px-Lion_%28Panthera_leo%29_%2830941994012%29.jpg';
 }
