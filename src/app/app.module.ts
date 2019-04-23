591c7ed9aaf53807e6215d2f103f1b6099007c8b
Francisco Romulo Freires do Nascimento <romulofreires1@gmail.com>
2019-04-23 10:27:00 -0300

    tratando rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PhotosMudule } from './photos/photos.module';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { PhotosMudule } from './photos/photos.module';
   ],
   imports: [
     BrowserModule,
-    PhotosMudule
+    PhotosMudule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Photo } from "./photo";
 
 const API = 'http://localhost:3000'
