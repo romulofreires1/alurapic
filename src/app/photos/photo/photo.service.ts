95c6ef2c8cb1fd67423f20f0fb8b499c7a8b956c
Rômulo <romulofreires1@gmail.com>
2019-04-23 20:52:07 -0300

    load button

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 import { Photo } from "./photo";
 
 const API = 'http://localhost:3000'
@@ -13,5 +14,12 @@ export class PhotoService {
         return this.http
         .get<any[]>(API + '/' + userName + '/photos')
 
+    }
+    listFromUserPaginated(userName: string, page: number) {
+        const params = new HttpParams()
+            .append('page', page.toString());
+    
+        return this.http
+            .get<Photo[]>(API + '/' + userName + '/photos', { params: params });
     }
 }
