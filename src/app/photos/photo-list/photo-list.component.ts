c27a779157692c3c995bbf51d7a1200a3eda8ded
Rômulo <romulofreires1@gmail.com>
2019-04-24 07:04:39 -0300

    modulo search

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 
 
 import { Photo } from '../photo/photo';
@@ -12,7 +11,7 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [
   ];
@@ -29,13 +28,6 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   ngOnInit(): void {   
     this.userName = this.activatedRoute.snapshot.params.userName; 
     this.photos = this.activatedRoute.snapshot.data.photos
-    this.debounce
-    .pipe(debounceTime(300))
-    .subscribe(filter => this.filter = filter);
-  }
-
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe();
   }
 
   load() {
